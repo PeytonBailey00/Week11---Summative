9d02cfca5e14a34ffd78566a9d83cfeee1ea107e
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-03-30 14:53:39 -0500

    Third Commit. Redid Player & Computer Functions.

--- app.js
+++ app.js
@@ -9,160 +9,53 @@ function welcome(a) {
 
 welcome(askname())
 
-function rulesQ() {
-    var ques1 = prompt("Do you know how to play Blackjack?")
-    return ques1
+function playerTurn() {
+
+    var pTotal = 0;
+
+    do {
+        var nextNum = Math.floor(Math.random() * 11)
+        var myTotal = myTotal + nextNum
+        alert(`Your number is ${nextNum}. Your total is now ${myTotal}. `)
+        var nextNumQ = prompt(`Would you like to pick another number?`)
+    } while (nextNumQ != "no") 
+
+    return pTotal
 }
 
-var rules = rulesQ()
+function computerTurn() {
 
-function knowledge() {
-    alert("Awesome! Let's get started!")
+    var cTotal = 0;
+
+    do {
+        var cNextN = Math.floor(Math.random() * 11)
+        var cTotal = cNextN
+        alert(`My number is ${cNextN}. My total is now ${cTotal}.`)
+        var cFinal = cTotal + cNextN
+    } while (cTotal < 16)
+
+    return cFinal
 }
 
-function education1() {
-    alert("Ok. So what we're going to do is we're both going to draw cards.")
-}
-
-function education2() {
-    alert("I will stop once I reach 17 or higher, but you can go as high as you want.")
-}
-
-function education3() {
-    alert("But remember, try to stay under 21.")
-}
-
-function starter1() {
-    var ready = prompt("Are you ready?")
-    return ready
-}
-
-var readyQ = starter1()
-
-function starter2() {
-    alert("Too bad.")
-}
-
-function myStart() {
-    alert("I'll let you go first. We aren't following Casino rules.")
-}
-
-// Player's Turn Starts Here
-
-function myPoint() {
-    var myPoints = 0
-    return myPoints
-}
-
-function cPoint() {
-    var cPoints = 0
-    return cPoints
-}
-
-function myMath1() {
-    var myNumA = Math.floor(Math.random() * 11)
-    return myNumA
-}
-
-var myNum1 = myMath1()
-
-function myNumAlert1() {
-    alert(`Your First card is ${myNum1}`)
-}
-
-function myMath2() {
-    var myNumB = Math.floor(Math.random() * 11)
-    return myNumB
-}
-
-var myNum2 = myMath2()
-
-function myNumAlert2() {
-    alert(`Your Second card is ${myNum2}`)
-}
-
-function myStartTotal() {
-    var myTotal = myNum1 + myNum2
-    return myTotal
-}
-
-var myT = myStartTotal()
-
-function myStartAlert() {
-    var nextNum = prompt(`Your total is ${myT}. Would you like to draw another card?`)
-    return nextNum
-}
-
-// Drawing Cards Do While Loop Functions Start Here
-
-function doNum() {
-    var nextNum = Math.floor(Math.random() * 11)
-    return nextNum
-}
-
-var nNum = doNum()
-
-function doNumA() {
-    alert(`Your next card is ${nNum}.`)
-}
-
-var nextT = nNum + myT
-
-function nextTotal() {
-    var numAgain = prompt(`Your total is now ${nextT}. Would you like to draw another card?`)
-    return numAgain
-}
-
-function myScoreOver() {
-    alert("Your score is over 20. I win.")
-}
-
-function losePoint() {
+function compare() {
+    if (myTotal > 20) {
+        alert(`Your score is over 20. I win.`)
     var cPoints = cPoints + 1
-    return cPoints
+    var myScoreOver = prompt("Would you like to play again?");
+    } if ((nextNumQ != "yes") && (myTotal < 21)) {
+        alert(`Ok. Your Final Total is ${myTotal}.`)
+    }
+    if (((myTotal > cTotal) && (myTotal <= 20) && (cTotal < 20))) {
+        alert(`Ok. I got ${cTotal}, and you got ${myTotal}. You Win!`)
+        var myPoints = myPoints + 1
+        var win = prompt("Would you like to play again?")
+    } else if (((cTotal > myTotal) && (cTotal <= 20) && (myTotal < 20))) {
+        alert(`Ok. I got ${cTotal}, and you got ${myTotal}. You Lose!`)
+        var cPoints = cPoints + 1
+        var lose = prompt("Would you like to play again?")
+    }
 }
 
-function playAgainA() {
-    var lose = prompt("Would you like to play again?")
-    return lose
-}
-
-
-
-
-
-
-
-
-
-
-if (rules == "yes") {
-    knowledge()
-} else if (rules == "no") {
-    education1()
-    education2()
-    education3()
-    starter1()
-}
-
-if (readyQ == "no") {
-    starter2()
-} else if (readyQ == "yes") {
-    myStart()
-}
-
-myPoint()
-cPoint()
-myMath1()
-myNumAlert1()
-myMath2()
-myNumAlert2()
-myStartTotal()
-myStartAlert()
-
-do {
-    doNum()
-    doNumA()
-    nextTotal()
-} while ((nextNum == "yes") && (numAgain == "yes"))
-
+playerTurn()
+computerTurn()
+compare()

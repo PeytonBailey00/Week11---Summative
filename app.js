30f7c256890a20fd1563c5caff16d8e380093f6b
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-03-31 14:44:30 -0500

    Fifth Commit. Final Grade - 90.

--- app.js
+++ app.js
@@ -15,13 +15,16 @@ function playerTurn() {
 
     do {
         var nextNum = Math.floor(Math.random() * 11)
-        var myTotal = myTotal + nextNum
-        alert(`Your number is ${nextNum}. Your total is now ${myTotal}. `)
+        var pTotal = nextNum + pTotal
+        alert(`Your number is ${nextNum}.`)
+        alert(`Your total is now ${pTotal}`)
         var nextNumQ = prompt(`Would you like to pick another number?`)
     } while (nextNumQ != "no") 
 
     return pTotal
 }
+
+var playerT = playerTurn()
 
 function computerTurn() {
 
@@ -29,33 +32,40 @@ function computerTurn() {
 
     do {
         var cNextN = Math.floor(Math.random() * 11)
-        var cTotal = cNextN
-        alert(`My number is ${cNextN}. My total is now ${cTotal}.`)
-        var cFinal = cTotal + cNextN
+        var cTotal = cTotal + cNextN
+        alert(`My number is ${cNextN}.`)
+        alert(`My total is now ${cTotal}`)
     } while (cTotal < 16)
 
-    return cFinal
+    return cTotal
+
 }
 
 function compare() {
-    if (myTotal > 20) {
+    if (playerT > 20) {
         alert(`Your score is over 20. I win.`)
     var cPoints = cPoints + 1
-    var myScoreOver = prompt("Would you like to play again?");
-    } if ((nextNumQ != "yes") && (myTotal < 21)) {
-        alert(`Ok. Your Final Total is ${myTotal}.`)
+    var myScoreOver = prompt("Would you like to play again?")
+
+    return myScoreOver
+    } if ((nextNumQ != "yes") && (playerT < 21)) {
+        alert(`Ok. Your Final Total is ${pTotal}.`)
     }
-    if (((myTotal > cTotal) && (myTotal <= 20) && (cTotal < 20))) {
-        alert(`Ok. I got ${cTotal}, and you got ${myTotal}. You Win!`)
+    if (((playerT > cTotal) && (playerT <= 20) && (cTotal < 20))) {
+        alert(`Ok. I got ${cTotal}, and you got ${playerT}. You Win!`)
         var myPoints = myPoints + 1
         var win = prompt("Would you like to play again?")
-    } else if (((cTotal > myTotal) && (cTotal <= 20) && (myTotal < 20))) {
-        alert(`Ok. I got ${cTotal}, and you got ${myTotal}. You Lose!`)
+
+        return win
+    } else if (((cTotal > playerT) && (cTotal <= 20) && (playerT < 20))) {
+        alert(`Ok. I got ${cTotal}, and you got ${playerT}. You Lose!`)
         var cPoints = cPoints + 1
         var lose = prompt("Would you like to play again?")
+
+        return lose
     }
 }
 
 playerTurn()
-computerTurn()
+
 compare()
